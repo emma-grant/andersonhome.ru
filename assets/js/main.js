bbe264895bc8d89d494df665b199d7412b972ecc
Emma Grant <emma-grant@list.ru>
2022-06-21 00:38:44 +0300

    save-block-about

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,35 +1,3 @@
-// $(function() {
-//  let headers = $('.headers_fixed');
-//  let headersHeight = headers.height(); // вычисляем высоту шапки
-  
-//  $(window).scroll(function() {
-//    if($(this).scrollTop() > 0) {
-//     headers.addClass('headers_fixed');
-//     $('body').css({
-//        'paddingTop': headersHeight+'px' // делаем отступ у body, равный высоте шапки
-//     });
-//    } else {
-//     headers.removeClass('headers_fixed');
-//     $('body').css({
-//      'paddingTop': 0 // удаляю отступ у body, равный высоте шапки
-//     })
-//    }
-//  });
-// });
-
-// if($(this).scrollTop() > 1) {
-//  $('.headers')({
-//    'padding': '5px 0',
-//    'background': '#25272c9a',
-//    'transition': '.3s'
-//  });
-// } else {
-//  $(".headers").css({
-//    'padding': '15px 0',
-//    'background': 'red',
-//    'transition': '.3s'
-//  });
-// }
 
 var scrolled;
 window.onscroll = function() {
